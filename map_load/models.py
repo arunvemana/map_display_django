120ec3d5480a9b49dc67e58fa6854a88142395d0
Arun sai Vemana <arunsai4b6@gmail.com>
2020-05-29 12:57:46 +0530

    small change to model.py to display list view in the admin panel

--- map_load/models.py
+++ map_load/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Map_List(models.Model):
     location_name = models.CharField(max_length=100)
     map_coordinates_link = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.location_name
